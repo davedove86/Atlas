c639d24f2ee11f8d88ec176df9ef54c04cc8f25b
Dave Dove <info@dave-dove.com>
2019-11-23 17:02:59 +0000

    header and navigation complete

---
+++ dist/assets/js/functions-min.js
@@ -0,0 +1,1 @@
+let mainNav=document.getElementById("js-menu"),navBarToggle=document.getElementById("js-navbar-toggle");navBarToggle.addEventListener("click",(function(){mainNav.classList.toggle("active")}));
