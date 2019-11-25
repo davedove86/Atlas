f3ee707bed1914c4b1ea93c10fffafcc6c6c5128
Dave Dove <info@dave-dove.com>
2019-11-25 21:50:27 +0000

    About us section

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,6 +1,33 @@
+// Navigation
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+// Tabs
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    // tablinks[i].style.backgroundColor = "red";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "flex";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").style.background = "#5ec6cf";
